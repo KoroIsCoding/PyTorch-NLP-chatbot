47ec9f6cc1b2e7009a9fd2787601d0771bc48092
KoroIsCoding <yh3575@columbia.edu>
2023-04-04 17:51:06 -0400

    Update Model
    Change structure of model

---
+++ tests/test_models.py
@@ -0,0 +1,11 @@
+# Authors: Koro_Is_Coding
+# License: Apache v2.0
+
+# standard libraries
+from collections import defaultdict
+
+# external libraries
+import pytest
+
+# module to be tested
+from src import SearchEngineStruct
