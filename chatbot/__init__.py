c643fb1a4aca2b1f198bc725e201d2f69bb74826
KoroIsCoding <yh3575@columbia.edu>
2023-05-12 01:11:52 -0400

    fix bugs

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -1,6 +1,8 @@
 from .models import SearchEngineStruct, SearchException
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 __all__ = [
     "SearchEngineStruct",
     "SearchException",
+    "TfidfVectorizer"
 ]
