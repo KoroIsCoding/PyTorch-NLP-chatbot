38ad998f20e051cea3e15cf68d13dc3a8e68dde4
KoroIsCoding <yh3575@columbia.edu>
2023-05-12 18:03:37 -0400

    HW7
    1. Code documentation
    2. docs: about Sphinx auto-docs for website

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,5 +1,6 @@
 # Authors: Koro_Is_Coding
 # License: Apache v2.0
+# Version: v.0.0.1
 
 import requests
 from string import Template
@@ -9,10 +10,36 @@ import operator
 
 
 class SearchException(Exception):
+    """
+    Exception raised when there are not enough search results.
+
+    This exception is typically raised when the number of items in a search response is less than 10.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
     print("no enough results")
 
 
 def cal_tfidf(results):
+    """
+    Calculate the TF-IDF scores for the words in the given search results.
+
+    Each search result should be a dictionary with the keys "title" and "snippet".
+    The words in these fields will be tokenized and used to calculate the TF-IDF scores.
+
+    Args:
+        results (list): A list of search result dictionaries. Each dictionary must have 'title'
+                        and 'snippet' keys.
+
+    Returns:
+        dict: A dictionary mapping each word to its TF-IDF score.
+
+    Raises:
+        ValueError: If any of the dictionaries in the results list does not contain 'title'
+                    or 'snippet' as keys.
+    """
     words = []
     for result in results:
         words.extend(re.sub("[^\w]", " ", result["title"]).split())
@@ -29,6 +56,16 @@ class SearchEngineStruct:
         google_api_key="AIzaSyAGHYwrgMmAHlFwM-GmS2anQ7xWu7qcIjA",
         google_engine_key="7a8bd65adc0b760d8",
     ):
+        """
+        Initialize the SearchEngineStruct with the given API keys.
+
+        Args:
+            google_api_key (str): The API key to use for Google's API. Default is an example key.
+            google_engine_key (str): The engine key to use for Google's API. Default is an example key.
+
+        Returns:
+            None
+        """
         self.google_api_key = google_api_key
         self.google_engine_key = google_engine_key
         self.top_result = list()
@@ -37,15 +74,62 @@ class SearchEngineStruct:
         self.query = ""
 
     def get_top_result(self):
+        """
+        Returns a copy of the top search results.
+
+        This function does not modify the state of the SearchEngineStruct object.
+
+        Args:
+        None
+
+        Returns:
+            list: A copy of the list of the top search results.
+        """
         return list(self.top_result)
 
     def set_query(self, seed_query):
+        """
+        Sets the query of the SearchEngineStruct object.
+
+        This function modifies the state of the SearchEngineStruct object by updating the query.
+
+        rgs:
+            seed_query (str): The new query string.
+
+        Returns:
+            None
+        """
         self.query = seed_query
 
     def get_query(self):
+        """
+        Returns the current query of the SearchEngineStruct object.
+
+        This function does not modify the state of the SearchEngineStruct object.
+
+        Args:
+            None
+
+        Returns:
+            str: The current query string.
+        """
         return self.query
 
     def call_google_custom_search_api(self):
+        """
+        Call the Google Custom Search API with the current query.
+
+        This function updates the top_result attribute with the results returned by the API.
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            SearchException: If the API response is None, or if there are less than 10 items in the response.
+        """
         URL = Template("https://www.googleapis.com/customsearch/v1?key=$client_key&cx=$engine_key&q=$query")
         url = URL.substitute(
             client_key=self.google_api_key,
@@ -102,6 +186,19 @@ class SearchEngineStruct:
         return accuracy
 
     def add_and_reorder_words(self):
+        """
+        Update the query based on the relevance feedback.
+
+        This function calculates the TF-IDF scores for the words in the relevant and irrelevant results,
+        finds the words that are unique to the relevant results, and adds the top two unique words to the query.
+        This function has a side effect of updating the query attribute.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         query_old = self.query
         # Calculate TF-IDF scores for words in relevant and irrelevant results
         relevant_tfidf = cal_tfidf(self.relevant)
--- chatbot/tests/test_models.py
+++ chatbot/tests/test_models.py
@@ -14,6 +14,11 @@ from chatbot import SearchException
 
 
 def test_se_default_init():
+    """
+    Test the default initialization of SearchEngineStruct.
+
+    This test checks if the default attributes of the SearchEngineStruct object are set correctly.
+    """
     se = SearchEngineStruct()
     assert se.google_api_key == "AIzaSyAGHYwrgMmAHlFwM-GmS2anQ7xWu7qcIjA"
     assert se.google_engine_key == "7a8bd65adc0b760d8"
@@ -24,6 +29,12 @@ def test_se_default_init():
 
 
 def test_se_api_init():
+    """
+    Test the initialization of SearchEngineStruct with provided API keys.
+
+    This test checks if the attributes of the SearchEngineStruct object are set correctly when
+    the google_api_key and google_engine_key arguments are provided.
+    """
     google_api_key = "api"
     google_engine_key = "engine"
     se = SearchEngineStruct(google_api_key, google_engine_key)
@@ -36,11 +47,23 @@ def test_se_api_init():
 
 
 def test_get_top_result_empty():
+    """
+    Test the get_top_result method of SearchEngineStruct when top results are empty.
+
+    This test checks if the get_top_result method correctly returns an empty list when no search
+    has been performed yet.
+    """
     se = SearchEngineStruct()
     assert len(SearchEngineStruct.get_top_result(se)) == 0
 
 
 def test_set_and_get_query():
+    """
+    Test the set_query and get_query methods of SearchEngineStruct.
+
+    This test checks if a query string can be successfully set and retrieved using the
+    set_query and get_query methods, respectively.
+    """
     se = SearchEngineStruct()
     seed_query = "test_query"
     se.set_query(seed_query)
@@ -48,6 +71,12 @@ def test_set_and_get_query():
 
 
 def test_search_current_query_google_success():
+    """
+    Test the call_google_custom_search_api method of SearchEngineStruct with a valid query.
+
+    This test checks if the call_google_custom_search_api method correctly updates the top_result
+    attribute with at least 10 search results when provided with a valid query.
+    """
     se = SearchEngineStruct()
     seed_query = "per se"
     se.set_query(seed_query)
@@ -56,6 +85,12 @@ def test_search_current_query_google_success():
 
 
 def test_search_current_query_google_fail():
+    """
+    Test the call_google_custom_search_api method of SearchEngineStruct with an invalid query.
+
+    This test checks if the call_google_custom_search_api method correctly raises a SearchException
+    when provided with a query that results in less than 10 search results.
+    """
     se = SearchEngineStruct()
     seed_query = "jjj sedskdsksdkdskdskdskdkssdkds"
     se.set_query(seed_query)
@@ -64,6 +99,15 @@ def test_search_current_query_google_fail():
 
 
 def test_choose_relevant_1(monkeypatch):
+    """
+    Test the choose_relevant method of SearchEngineStruct when all search results are relevant.
+
+    This test checks if the choose_relevant method correctly updates the relevant and irrelevant
+    attributes and returns an accuracy of 1 when all search results are considered relevant by the user.
+
+    Args:
+        monkeypatch: A fixture provided by pytest for mocking.
+    """
     number_inputs = StringIO("Y\n" * 10)
     se = SearchEngineStruct()
     seed_query = "per se"
@@ -76,6 +120,15 @@ def test_choose_relevant_1(monkeypatch):
 
 
 def test_choose_relevant_0(monkeypatch):
+    """
+    Test the choose_relevant method of SearchEngineStruct when no search results are relevant.
+
+    This test checks if the choose_relevant method correctly updates the relevant and irrelevant
+    attributes and returns an accuracy of 0 when no search results are considered relevant by the user.
+
+    Args:
+        monkeypatch: A fixture provided by pytest for mocking.
+    """
     number_inputs = StringIO("N\n" * 10)
     se = SearchEngineStruct()
     seed_query = "per se"
